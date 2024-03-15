952d1e681571a7b697b25868ec66480ba506aa2e
BoraErenErdem <erdemeren3460@gmail.com>
2024-03-15 12:34:04 +0300

    The goal is to progress step by step from basic to advanced levels by solving problems systematically.

--- Number_guessing_game_basic.py
+++ Number_guessing_game_basic.py
@@ -24,3 +24,30 @@ except ValueError as err:
 finally:
     print("Game Over")
 # endregion
+
+
+
+# region 10 Defa Tahmin Hakkı Olan Sayı Tahmin Oyunu
+from random import randint
+try:
+    rastgele_sayi = randint(1,100)
+    print("Sayı tahmin oyununa hoşgeldiniz.")
+    deneme = 0
+    Hak = 10
+    while True:
+        oyuncu = int(input("Sayı tahmini yapınız:"))
+        deneme += 1
+        Hak -= 1
+        if rastgele_sayi == oyuncu:
+            print(f'Tebrikler.{deneme} denemede kalan {Hak} hakkınızı kullanmadan bildiniz!')
+            break
+        elif rastgele_sayi < oyuncu:
+            print("Daha küçük bir sayı giriniz..")
+        elif rastgele_sayi > oyuncu:
+            print("Daha büyük bir sayı giriniz..")
+        if Hak == 0:
+            print(f'Bütün hakkınızı kullandınız. Tutulan sayı: {rastgele_sayi}')
+            break
+except ValueError as err:
+    print(f'Lütfen geçerli parametreleri giriniz..!  {err}')
+# endregion
